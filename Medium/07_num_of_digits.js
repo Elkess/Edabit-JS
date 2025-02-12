b01121f93036cd91003c9d5b9674c50935cec46b
MOHAMMED AMINE ELKESS <98390636+Elkess@users.noreply.github.com>
2025-02-12 09:40:06 +0000

    Find Number of Digits in Number

---
+++ Medium/07_num_of_digits.js
@@ -0,0 +1,13 @@
+// https://edabit.com/challenge/yFJzLfYghz7ZtsyAN
+// Find Number of Digits in Number
+
+function num_of_digits(num) {
+	let	count = 1 * (num == 0);
+	while(num)
+	{
+		num = parseInt(num /10);		
+		count++;
+	}
+	return (count);
+}
+console.log(num_of_digits(-59525));
