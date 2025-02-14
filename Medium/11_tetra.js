3041f5f45ca57b47940764d870745ff905ba84f8
MOHAMMED AMINE ELKESS <98390636+Elkess@users.noreply.github.com>
2025-02-14 15:50:48 +0000

    Find the nth Tetrahedral Number

---
+++ Medium/11_tetra.js
@@ -0,0 +1,9 @@
+// https://edabit.com/challenge/zRCyxKBBmr4F2x4Bv
+// Find the nth Tetrahedral Number
+// Tn = (n * (n + 1) * (n + 2)) / 6
+
+function tetra(n) {
+	return ((n * (n+1) * (n+2)) / 6)
+}
+
+console.log(tetra(2));
