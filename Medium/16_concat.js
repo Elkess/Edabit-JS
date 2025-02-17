456a58bc46d73d58c24b8db1f6a2986a5c572419
MOHAMMED AMINE ELKESS <98390636+Elkess@users.noreply.github.com>
2025-02-17 15:44:09 +0000

    Concatenate Variable Number of Input Arrays

---
+++ Medium/16_concat.js
@@ -0,0 +1,20 @@
+// https://edabit.com/challenge/Hs4TpDHX57Rqegmgg
+// Concatenate Variable Number of Input Arrays
+
+// function concat(...args)
+// {
+// 	let arr = arguments[0];
+// 	for (let i = 1; i < arguments.length; i++) {
+// 		arr = arr.concat(arguments[i]);
+// 	}
+// 	return arr
+// }
+
+// function concat(...args) {
+//     return args.reduce((acc, arr) => acc.concat(arr), []);
+// }
+
+function concat(...args) {
+    return args.flat();
+}
+console.log(concat([1, 2],[5]));
