f46607c55e2ace4bf509b1a15457adb5d394bf3a
MOHAMMED AMINE ELKESS <98390636+Elkess@users.noreply.github.com>
2025-02-17 15:42:49 +0000

    Function Factory

---
+++ Medium/13_makePlusFunction.js
@@ -0,0 +1,13 @@
+// https://edabit.com/challenge/ENWFBL4jbTgLbSqwS
+// Function Factory
+
+function makePlusFunction(baseNum) {
+	return (arg)=> baseNum + arg ;
+}
+
+const plusTwo = makePlusFunction(2);
+const plusFive = makePlusFunction(5);
+const plusSeven = makePlusFunction(plusTwo(plusFive(0)));
+const plusTen = makePlusFunction(10);
+
+console.log(plusFive(10));
