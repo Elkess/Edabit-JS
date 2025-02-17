978b740c514e20bb13f8cc4aa5a237cbbcd60839
MOHAMMED AMINE ELKESS <98390636+Elkess@users.noreply.github.com>
2025-02-17 15:42:10 +0000

    Learn Lodash (2): Compact

---
+++ Medium/12_compact.js
@@ -0,0 +1,11 @@
+// https://edabit.com/challenge/M6fbYyBkzJXMAu39G
+// Learn Lodash (2): Compact
+
+const arr = [1,0,false,null,undefined,"banana"];
+// function compact(arr) {
+// 	return (arr.filter((element)=> !!element))
+// }
+function compact(arr) {
+	return (arr.filter((element)=> !!element))
+}
+console.log(compact(arr));
