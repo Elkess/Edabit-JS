ae48a93ba81d5370940e6de3ee50abc5435686fc
MOHAMMED AMINE ELKESS <98390636+Elkess@users.noreply.github.com>
2025-02-17 15:43:42 +0000

    Converting Objects to Arrays

---
+++ Medium/15_toArray.js
@@ -0,0 +1,8 @@
+// https://edabit.com/challenge/pPNAs5PvB3WvnDwDM
+// Converting Objects to Arrays
+let  obj = { a: 1, b: 2 };
+obj ={ foo: 33, bar: 45, baz: 67 };
+function toArray(obj) {
+	return Object.entries(obj) 
+}
+console.log(obj);
