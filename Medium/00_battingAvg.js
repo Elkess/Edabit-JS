16b107b50f48b6bf3e35ad8916c51acd4828baf6
MOHAMMED AMINE ELKESS <98390636+Elkess@users.noreply.github.com>
2025-02-19 09:36:17 +0000

    Baseball Batting Average

---
+++ Medium/00_battingAvg.js
@@ -0,0 +1,21 @@
+// https://edabit.com/challenge/MEZtqvEeMbGg2YKtt
+// Baseball Batting Average
+
+function battingAvg(arr) {
+	let hits=0;
+	let atbats=0;
+	arr.forEach((e) => {
+		hits += e[0];
+		atbats += e[1];
+	});
+	let average = hits/atbats;
+	return average.toFixed(3).toString().substring(1);
+}
+
+console.log(battingAvg([
+	[0, 0],
+	[1, 3],
+	[2, 2],
+	[0, 4],
+	[1, 5],
+]));
