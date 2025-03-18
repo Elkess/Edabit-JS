4cb6aba241034ed2065e1d332d9a192a4342754f
MOHAMMED AMINE ELKESS <98390636+Elkess@users.noreply.github.com>
2025-03-18 16:34:09 +0000

    Seven Boom!

---
+++ Hard/00_sevenBoom.js
@@ -0,0 +1,14 @@
+// https://edabit.com/challenge/6R6gReGTGwzpwuffD
+// Seven Boom!
+
+function sevenBoom(arr) {
+	let i = 0;
+	for(i = 0; i < arr.length; i++)
+		{
+			if (arr[i].toString().includes('7'))
+				return ("Boom!");
+		}
+	return ("there is no 7 in the array")
+}
+
+console.log(sevenBoom([5, 675, 21]));
