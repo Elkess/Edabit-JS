5a2d3ff7b764d2e3afc48d09fe3adc14e9a0c15e
MOHAMMED AMINE ELKESS <98390636+Elkess@users.noreply.github.com>
2025-03-19 08:47:04 +0000

    Number of Boomerangs

---
+++ Hard/02_countBoomerangs.js
@@ -0,0 +1,20 @@
+// https://edabit.com/challenge/b7iHQDw72zzkmgCun
+// Number of Boomerangs
+
+let arr = [9, 5, 9, 5, 1, 1, 1];
+arr = [3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2];
+arr = [1, 7, 1, 7, 1, 7, 1];
+arr = [4, 4, 4, 9, 9, 9, 9];
+function countBoomerangs(arr) {
+	let len = arr.length;
+	let i = 0;
+	let count = 0;
+	while (i < len -2)
+	{
+		if (arr[i] == arr[i+2] && arr[i +1] != arr[i])
+			count++;
+		i++;
+	}
+	return (count);
+}
+console.log(countBoomerangs(arr));
