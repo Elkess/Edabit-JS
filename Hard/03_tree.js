c1c7eecbdc7aef497ac9e67d311b209263f50206
MOHAMMED AMINE ELKESS <98390636+Elkess@users.noreply.github.com>
2025-04-05 20:05:46 +0000

    Christmas Tree

---
+++ Hard/03_tree.js
@@ -0,0 +1,20 @@
+// https://edabit.com/challenge/QeaCDhw3QrLwbeteD
+// Christmas Tree
+
+function tree(h) {
+	const hash = "#";
+	const space = " ";
+	let arr = [];
+	let i = 1;
+	while (h--)
+	{
+		arr.push(
+			space.repeat(h) + 
+			hash.repeat(i) +
+			space.repeat(h)  
+		);
+		i += 2;
+	}
+	return (arr)
+}
+console.log(tree(5));
