0647dc1b192eacd9d5bc75c3776f4f21f76de429
MOHAMMED AMINE ELKESS <98390636+Elkess@users.noreply.github.com>
2025-04-08 09:49:01 +0000

    Length of a Nested Array

---
+++ Hard/05_getLength.js
@@ -0,0 +1,19 @@
+// https://edabit.com/challenge/yXSTvCNen2DQHyrh6
+// Length of a Nested Array
+
+function getLength(arr)
+{
+	let i = 0;
+	let count = 0;
+	let arra = arr.toString();
+	while (arra[i])
+	{
+		if (arra[i] != ",")
+			count++;
+		i++;
+	}
+	return count;
+}
+// console.log(getLength([1, [3, 4], [5]]));
+console.log(getLength([1, [2, [3, [4, [5, 6]]]]]));
+
